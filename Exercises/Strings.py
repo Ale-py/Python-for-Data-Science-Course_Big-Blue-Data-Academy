7469d692f3e3eae74d245b828246f572797dfd49
Alexandros Papanikos <92908848+Ale-py@users.noreply.github.com>
2025-06-07 09:19:06 +0300

    Update Strings.py

--- Exercises/Strings.py
+++ Exercises/Strings.py
@@ -1,44 +1,31 @@
 # 1. Create a variable greeting that stores the string "Hello, world!" and print it.
-'''
 greeting = "Hello, world!"
 print(greeting)
-'''
 # 2. Define two variables, first_name and last_name, each containing a string. Concatenate
 # them with a space in between to form a new variable full_name, and then print it.
-'''
 first_name = "Siera"
 last_name = "Pier"
 full_name = first_name + " " + last_name
 print(full_name)
-'''
 # 3. Assign the string "Python" to a variable. Use indexing to print the first character and the last character of the string.
-'''
 newString = "Python"
 print(newString[0] + newString[-1])
-'''
 # 4. Using the string "Python", extract a substring (for example, "ytho") using slicing, and print the result.
-'''
 slicing = "Python"
 print(slicing[1:5])
-'''
 # 5. Create a variable word with a string of your choice. Use the len() function to print the length of the string.
-'''
 word = "What's up?"
 print(len(word))
-'''
 # 6. Assign a string to a variable. Convert the string to uppercase using the upper() method and
 # to lowercase using the lower() method, then print both results.
-'''
 word = "HeLlo WoRld!"
 print(word + " " + word.lower()+ " " + word.upper())
-'''
 # 7. Create a variable containing a sentence. Use the find() method to locate
 # a specific word within the sentence and print the index where it first appears.
-'''
 sentence = "My name is Nemo and i like games and i like food."
 print(sentence.find("and"))
 print(sentence.find("and",17))
-'''
+
 
 
 
