f7f72f02d6576ab5291df8e6b45240d46167aefd
Alexandros Papanikos <92908848+Ale-py@users.noreply.github.com>
2025-06-07 09:16:07 +0300

    Update Loops.py

--- Exercises/Loops.py
+++ Exercises/Loops.py
@@ -1,30 +1,22 @@
 # 1. Write a for loop that prints each number from 0 to 4 using the range() function.
-'''
 for num in range(5):
     print(num)
-'''
 # 2. Given a list colors = ["red", "green", "blue"], write a for loop that prints each color.
-'''
 colors = ["red", "green", "blue"]
 for col in colors:
     print(col)
-'''
 # 3. Initialize a variable count = 1 and write a while loop that prints numbers from 1 to 5, incrementing count by 1 each time.
-'''
 count = 1
 while count <= 5:
     print(count)
     count += 1
-'''
 # 4. Create a list of numbers (e.g., [1, 2, 3, 4, 5]) and use a for loop to calculate and print the sum of the list.
-'''
 count = 0
 numbers = [5, 15, 20, 5, 5]
 for num in numbers:
     count += num
 print(count)
 #print(sum(numbers))
-'''
 
 
 
