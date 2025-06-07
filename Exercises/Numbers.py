02585596e0dfb16f8930386c3350b158655f9232
Alexandros Papanikos <92908848+Ale-py@users.noreply.github.com>
2025-06-07 09:17:00 +0300

    Update Numbers.py

--- Exercises/Numbers.py
+++ Exercises/Numbers.py
@@ -1,37 +1,28 @@
 # 1. Create two variables, x and y, each containing an integer value. Create a third variable z that is equal to the sum of x and y.
-'''
 x = 4
 y = 5
 z = x + y
 print(z)
-'''
 # 2. Create a variable x containing an integer value. Transform that value into a float and save it under the same variable x
-'''
 x = 4
 print(x)
 x = float(x)
 print(x)
-'''
 # 3. Define two variables, a and b, with integer values. Multiply them together and store the result in a new variable c.
-'''
 a = 9
 b = 8
 c = a * b
 print(c)
-'''
 # 4. Assign two integers to variables num1 and num2. Write an expression that
 # calculates the remainder when num1 is divided by num2 and store the result in a variable called remainder.
-'''
 num1 = 47
 num2 = 2
 remainder = num1 % num2
 print(remainder)
-'''
 # 5. Create variables base and exponent with integer values. Calculate base raised to
 # the power of exponent and assign it to a variable named result.
-'''
 base = 4
 exponent = 3
 result = base ** exponent
 print(result)
-'''
+
